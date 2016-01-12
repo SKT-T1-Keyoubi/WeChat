efdcc6d3a45c5bb6e8e081841905b0c1bacdd0d1
LoveQiuYi <loveqiuyi@Zhang-Xin-Mac-mini.local>
2016-01-12 10:42:53 +0800

    新建一个github项目

---
+++ WeChat/WeChat/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  WeChat
+//
+//  Created by LoveQiuYi on 16/1/12.
+//  Copyright © 2016年 LoveQiuYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ WeChat/WeChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChat
+//
+//  Created by LoveQiuYi on 16/1/12.
+//  Copyright © 2016年 LoveQiuYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
